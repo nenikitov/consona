9ac6ac822fc65cd75c93fb5d73f60537be04cff8
nenikitov <nenikitov@mail.ru>
2025-01-02 23:45:51 -0500

    refactor: quotes

--- modules/alacritty/default.nix
+++ modules/alacritty/default.nix
@@ -10,28 +10,28 @@ in
       programs.alacritty.settings = {
         colors = {
           primary = {
-            background = hex palette."bg";
-            foreground = hex palette."fg";
+            background = hex palette.bg;
+            foreground = hex palette.fg;
           };
           normal = {
-            black = hex palette."black";
-            red = hex palette."red";
-            green = hex palette."green";
-            yellow = hex palette."yellow";
-            blue = hex palette."blue";
-            magenta = hex palette."magenta";
-            cyan = hex palette."cyan";
-            white = hex palette."white";
+            black = hex palette.black;
+            red = hex palette.red;
+            green = hex palette.green;
+            yellow = hex palette.yellow;
+            blue = hex palette.blue;
+            magenta = hex palette.magenta;
+            cyan = hex palette.cyan;
+            white = hex palette.white;
           };
           bright = {
-            black = hex palette."standoutBlack";
-            red = hex palette."standoutRed";
-            green = hex palette."standoutGreen";
-            yellow = hex palette."standoutYellow";
-            blue = hex palette."standoutBlue";
-            magenta = hex palette."standoutMagenta";
-            cyan = hex palette."standoutCyan";
-            white = hex palette."standoutWhite";
+            black = hex palette.standoutBlack;
+            red = hex palette.standoutRed;
+            green = hex palette.standoutGreen;
+            yellow = hex palette.standoutYellow;
+            blue = hex palette.standoutBlue;
+            magenta = hex palette.standoutMagenta;
+            cyan = hex palette.standoutCyan;
+            white = hex palette.standoutWhite;
           };
           # TODO(nenikitov): Look into changing `selection` colors too
         };
--- modules/tty/default.nix
+++ modules/tty/default.nix
@@ -14,22 +14,22 @@ in
           # ${header}
           # TTY color scheme
           if [ "''${TERM}" = "linux" ]; then
-            echo -en "\e]P0${hexNoHash palette."bg"}"
-            echo -en "\e]P1${hexNoHash palette."red"}"
-            echo -en "\e]P2${hexNoHash palette."green"}"
-            echo -en "\e]P3${hexNoHash palette."yellow"}"
-            echo -en "\e]P4${hexNoHash palette."blue"}"
-            echo -en "\e]P5${hexNoHash palette."magenta"}"
-            echo -en "\e]P6${hexNoHash palette."cyan"}"
-            echo -en "\e]P7${hexNoHash palette."white"}"
-            echo -en "\e]P8${hexNoHash palette."standoutBlack"}"
-            echo -en "\e]P9${hexNoHash palette."standoutRed"}"
-            echo -en "\e]PA${hexNoHash palette."standoutGreen"}"
-            echo -en "\e]PB${hexNoHash palette."standoutYellow"}"
-            echo -en "\e]PC${hexNoHash palette."standoutBlue"}"
-            echo -en "\e]PD${hexNoHash palette."standoutMagenta"}"
-            echo -en "\e]PE${hexNoHash palette."standoutCyan"}"
-            echo -en "\e]PF${hexNoHash palette."fg"}"
+            echo -en "\e]P0${hexNoHash palette.bg}"
+            echo -en "\e]P1${hexNoHash palette.red}"
+            echo -en "\e]P2${hexNoHash palette.green}"
+            echo -en "\e]P3${hexNoHash palette.yellow}"
+            echo -en "\e]P4${hexNoHash palette.blue}"
+            echo -en "\e]P5${hexNoHash palette.magenta}"
+            echo -en "\e]P6${hexNoHash palette.cyan}"
+            echo -en "\e]P7${hexNoHash palette.white}"
+            echo -en "\e]P8${hexNoHash palette.standoutBlack}"
+            echo -en "\e]P9${hexNoHash palette.standoutRed}"
+            echo -en "\e]PA${hexNoHash palette.standoutGreen}"
+            echo -en "\e]PB${hexNoHash palette.standoutYellow}"
+            echo -en "\e]PC${hexNoHash palette.standoutBlue}"
+            echo -en "\e]PD${hexNoHash palette.standoutMagenta}"
+            echo -en "\e]PE${hexNoHash palette.standoutCyan}"
+            echo -en "\e]PF${hexNoHash palette.fg}"
           fi
         '';
     in {
